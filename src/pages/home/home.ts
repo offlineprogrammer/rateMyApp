920ec44ce900898b1adba9ac63f9377bfe959505
Offline Programmer <mmalaka@gmail.com>
2017-03-21 22:16:51 +0000

    RateApp

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+import { AppRate } from 'ionic-native';
+import { Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-    
+
+
+export class HomePage {
+  appRate: any = AppRate;
+constructor(public navCtrl: NavController,public platform: Platform) {
+      this.platform.ready().then(
+      () => this.appRate.preferences.storeAppURL = {
+        ios: '<Your_App_ID>',
+        android: 'market://details?id=<Your_App_ID>'
+      } 
+    )
   }
 
+
+  rateApp() {
+    AppRate.promptForRating(true);
+  }
 }
